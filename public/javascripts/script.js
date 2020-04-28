0dd1fd8250cdd9276984dbe29a44ed6a97430313
NormanHussey <norm.hussey@gmail.com>
2020-04-28 12:03:18 -0400

    removed form submit, changes now happen constantly, made some style improvements

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,12 +2,13 @@ const converter = new showdown.Converter(),
     text      = '# hello, markdown!',
     html      = converter.makeHtml(text);
 
-const input = document.querySelector('#input');
-const inputForm = document.querySelector('#inputForm');
+const input = document.querySelector('#inputText');
 const output = document.querySelector('.output');
 
-inputForm.addEventListener('submit', function (e) {
-    e.preventDefault();
+function updateOutput() {
     const markedInput = converter.makeHtml(input.value);
     output.innerHTML = markedInput;
-});
+    requestAnimationFrame(updateOutput);
+}
+
+window.requestAnimationFrame(updateOutput);
