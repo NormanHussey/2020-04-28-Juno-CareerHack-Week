364c365b138014d7f293c5f3cb1917e78b5ca99f
NormanHussey <norm.hussey@gmail.com>
2020-04-29 09:38:00 -0400

    successfully passing info through the server using GET and POST

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,14 +1,60 @@
-const converter = new showdown.Converter(),
-    text      = '# hello, markdown!',
-    html      = converter.makeHtml(text);
+const app = {};
 
-const input = document.querySelector('#inputText');
-const output = document.querySelector('.output');
+app.converter = new showdown.Converter();
+app.input = document.querySelector('#inputText');
+app.output = document.querySelector('.output');
+app.downloadBtn = document.querySelector('#downloadBtn');
+app.reader = new FileReader();
+app.form = document.querySelector('#inputForm');
+app.update = document.querySelector('#update');
 
-function updateOutput() {
-    const markedInput = converter.makeHtml(input.value);
-    output.innerHTML = markedInput;
-    requestAnimationFrame(updateOutput);
+app.download = () => {
+    // const savedJson = JSON.stringify(app.output.innerHTML);
+    app.saveToLocalDrive(app.output.innerHTML, 'mark-it.txt', 'text/plain');
 }
 
-window.requestAnimationFrame(updateOutput);
+app.load = (file) => {
+
+}
+
+app.saveToLocalDrive = (content, fileName, contentType) => {
+    let a = document.createElement("a");
+    let file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
+}
+
+app.downloadBtn.addEventListener('click', () => {
+    app.download();
+});
+
+app.reader.addEventListener('load', (e) => {
+    const loadedFileText = e.target.result;
+    app.load(loadedFileText);
+});
+
+app.form.addEventListener('submit', (e) => {
+    // e.preventDefault();
+    // fetch('/api/text')
+    // .then(response => {
+    //     app.output.innerText = response;
+    // });
+});
+
+update.addEventListener('click', (e) => {
+    fetch('/api/text')
+      .then(response => response.json())
+      .then(json => {
+        console.log(json.html);
+        app.output.innerHTML = json.html;
+      });
+});
+
+app.updateOutput = () => {
+    // const markedInput = app.converter.makeHtml(app.input.value);
+    // app.output.innerHTML = markedInput;
+    requestAnimationFrame(app.updateOutput);
+}
+
+// window.requestAnimationFrame(app.updateOutput);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
+const showdown  = require('showdown');
+const converter = new showdown.Converter();
+let data = {};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/api/text', function (req, res) {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.json(data);
+});
+
+router.post('/api/text', function(req, res) {
+  data.html = converter.makeHtml(req.body.inputText);
+  res.redirect('/');
 });
 
 module.exports = router;
